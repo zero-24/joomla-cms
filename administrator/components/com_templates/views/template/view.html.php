d80becb616c98792a16013a238935b1d464064bf
zero-24 <zero-24@users.noreply.github.com>
2017-04-18 15:30:22 +0200

    Revert "Fix for XSS in injecting the file name"
    This reverts commit bfa2567c3275bccad7d02131990450d315c2f5ea.

--- administrator/components/com_templates/views/template/view.html.php
+++ administrator/components/com_templates/views/template/view.html.php
@@ -97,7 +97,7 @@ class TemplatesViewTemplate extends JViewLegacy
 	{
 		$app            = JFactory::getApplication();
 		$this->file     = $app->input->get('file');
-		$this->fileName = JFilterInput::getInstance()->clean(base64_decode($this->file), 'string');
+		$this->fileName = base64_decode($this->file);
 		$explodeArray   = explode('.', $this->fileName);
 		$ext            = end($explodeArray);
 		$this->files    = $this->get('Files');
